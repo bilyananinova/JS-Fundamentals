c1110010fe9261bf49f834f5ebc515236b78eaf2
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-01-17 20:30:30 +0200

    Update arrayManipulator.js

--- Exercise Arrays Advanced/arrayManipulator.js
+++ Exercise Arrays Advanced/arrayManipulator.js
@@ -1,70 +1,78 @@
 function arrayManipulator(arrNum, arrString) {
 
-    for (let i = 0; i < arrString.length; i++) {
-        let element = arrString[i].split(' ');
-        let command = element.shift();
+    for (let command of arrString) {
 
-        if (command == 'add') {
+        command = command.split(" ");
+        let com = command.shift();
 
-            let index = element.shift();
-            let num = Number(element.shift());
+        if (com === 'add') {
 
-            arrNum.splice(index, 0, num);
+            let index = Number(command[0]);
+            let element = Number(command[1]);
+            arrNum.splice(index, 0, Number(element));
 
-        } else if (command == 'addMany') {
-            let index = Number(element.shift());
-            let num = element
+        } else if (com === 'addMany') {
 
-            for (let j = 0; j < num.length; j++) {
-                arrNum.splice(index, 0, Number(num[j]));
-                index++;
+            let index = command.shift();
+            let nums = command.map(Number)
+            arrNum.splice(Number(index), 0, ...nums);
+
+        } else if (com === 'contains') {
+
+            let element = Number(command);
+            console.log(arrNum.indexOf(element))
+
+        } else if (com === 'remove') {
+
+            let index = Number(command);
+            arrNum.splice(index, 1)
+
+        } else if (com === 'shift') {
+
+            let positions = Number(command);
+
+            for (let i = 0; i < positions; i++) {
+                let el = arrNum.shift();
+                arrNum.push(el);
             }
 
-        } else if (command == 'contains') {
+        } else if (com === 'sumPairs') {
+            let result = [];
 
-            let index = Number(element.shift());
-
-            if (arrNum.includes(Number(index))) {
-                let i = arrNum.indexOf(Number(index));
-                console.log(i);
+            if (arrNum.length % 2 === 0) {
+                for (let i = 0; i < arrNum.length; i += 2) {
+                    result.push(arrNum[i] + arrNum[i + 1]);
+                }
             } else {
-                console.log('-1');
+                for (let i = 0; i < arrNum.length - 1; i += 2) {
+                    result.push(arrNum[i] + arrNum[i + 1]);
+                }
+                result.push(arrNum.pop());
             }
 
-        } else if (command == 'remove') {
-            let index = element[0];
-            arrNum.splice(index, 1);
-        } else if (command == 'shift') {
-            let rot = element.shift();
+            arrNum = result;
 
-            for (let k = 0; k < rot; k++) {
-                let item = arrNum.shift()
-                arrNum.push(item)
-            }
-        } else if (command == 'sumPairs') {
+        } else if (com === 'print') {
 
-            let temporaryArr = [];
+            console.log('[ ' + arrNum.join(', ') + ' ]');
 
-            for (let m = 0; m < arrNum.length / 2; m++) {
-                let firstN = arrNum[m]
-                let secondN = arrNum[m + 1]
-                let sumPairs = firstN + secondN;
-                temporaryArr.push(sumPairs);
-
-            }
-
-            arrNum = temporaryArr;
-
-        } else if (command == 'print') {
-            break;
         }
     }
+    // console.log(arrNum);
 
-    console.log('[ ' + arrNum.join(', ') + ' ]');
 }
 
-// arrayManipulator([1, 2, 4, 5, 6, 7], ['add 1 8', 'contains 1', 'contains 3', 'print'])
-// arrayManipulator([1, 2, 3, 4, 5], ["addMany 5 9 8 7 6 5", "contains 15", "remove 3", "shift 1", "print"])
-// arrayManipulator([2, 2, 4, 2, 4], ["add 1 4", "sumPairs", "print"])
-// arrayManipulator([1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2], ["sumPairs", "sumPairs", "addMany 0 -1 -2 -3", "print"])
-arrayManipulator([1, 2, 3, 4, 5, 6], ["sumPairs", "sumPairs", "addMany 0 -1 -2 -3", "contains 15", 'add 1 8', "remove 3", "print"])
+arrayManipulator([1, 2, 4, 5, 6, 7], ['add 1 8', 'contains 1', 'contains 3', 'print']);
+console.log('*'.repeat(50));
+
+arrayManipulator([1, 2, 3, 4, 5], ["addMany 5 9 8 7 6 5", "contains 15", "remove 3", "shift 1", "print"]);
+console.log('*'.repeat(50));
+
+arrayManipulator([2, 2, 4, 2, 4], ["add 1 4", "sumPairs", "print"]);
+console.log('*'.repeat(50));
+
+arrayManipulator([1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2], ["sumPairs", "sumPairs", "addMany 0 -1 -2 -3", "print"]);
+console.log('*'.repeat(50));
+
+arrayManipulator([1, 2, 3, 4, 5, 6], ["sumPairs", "sumPairs", "addMany 0 -1 -2 -3", "contains 15", 'add 1 8', "remove 3", "print"]);
+
